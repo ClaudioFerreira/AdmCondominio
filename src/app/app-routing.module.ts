9eb21aa798a0f9dae67acab7da2ed9bd80a36687
Claudio Ferreira <claudio.zero0@gmail.com>
2022-01-31 16:45:44 -0300

    feat(firebase) include firebase dependence and guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AuthGuard } from './shared/guard/auth.guard';
+
 import { LoginComponent } from './auth/login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', component: AppComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'admcondominio-57fe2',
+    appId: '1:981318096012:web:293f6daa14fcc55ec017a9',
+    storageBucket: 'admcondominio-57fe2.appspot.com',
+    apiKey: 'AIzaSyBDt8_kLRVx7C9GqJPZ4Oo7_bcIJdRAg5Q',
+    authDomain: 'admcondominio-57fe2.firebaseapp.com',
+    messagingSenderId: '981318096012',
+    measurementId: 'G-52H3FG3ZG6',
+  },
   production: false
 };
 
