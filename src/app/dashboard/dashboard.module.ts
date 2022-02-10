5715a220cbb2b67b80cdf7bacce7303f6904ccc5
Claudio Ferreira <claudio.zero0@gmail.com>
2022-02-10 16:15:55 -0300

    feat(ngx-mask) add mask

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,6 +3,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { NgxMaskModule } from 'ngx-mask';
+
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { CasasComponent } from './../casas/casas.component';
@@ -20,7 +22,7 @@ import { CasasDetalhesComponent } from './../casas-detalhes/casas-detalhes.compo
     DashboardRoutingModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
-
+    NgxMaskModule.forRoot(),
   ]
 })
 export class DashboardModule { }
